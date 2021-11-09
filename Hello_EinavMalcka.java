0e3b6e170c8e670235864ea96bb99eec63940210
einav.malcka <einav.malcka@tufin.com>
2021-11-09 11:58:16 +0200

    create a change to trigger a build - Hello einav 20 times _ 5

--- Hello_EinavMalcka.java
+++ Hello_EinavMalcka.java
@@ -1,7 +1,7 @@
 public class Hello_EinavMalcka{
 	public static void main(String[] args) {
-		for (int i=1;i<=10; i++) {
-			System.out.println("Hello Einav again ..."+i);
+		for (int i=1;i<=20; i++) {
+			System.out.println("Hello Einav ..."+i);
 		}
 	}
 }
